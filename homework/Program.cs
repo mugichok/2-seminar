ad4338e9552033aabf3509e15bda89019e127a9c
gendfrodit <farengate20017@gmail.com>
2023-12-27 22:20:46 +0300

    corrections

--- homework/Program.cs
+++ homework/Program.cs
@@ -17,23 +17,16 @@
 // string number = Console.ReadLine();
 // int y = number.Length;
 // int x = int.Parse(number);
+// x = Math.Abs(x);
 // if(x<99)
 //     Console.WriteLine("Третьей цифры в числе нет");
 // else
 // {
-//     int s = 10;
-//     int result = 1;
-//     int result1 =1;
-//     for (int i = 0; i < y-2; i++)
+//     while(x>999)
 //     {
-//         result *= s;
+//         x /= 10;
 //     }
-//     int n = x%result;
-//     for (int i = 0; i < y-3; i++)
-//     {
-//         result1 *= s;
-//     }
-//     Console.WriteLine(n/result1);
+//     Console.WriteLine(x%10);
 // }
 
 // третья задача Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
@@ -53,35 +46,95 @@
 
 // четвёртая задача Задана последовательность натуральных чисел, завершающаяся числом 0. Требуется определить значение второго по величине элемента в этой последовательности, то есть элемента, который будет наибольшим, если из последовательности удалить наибольший элемент.
 
-int[] array = { 1, 7, 9, 0 };
-int length = array.Length;
-int max = 0;
-for (int i = 0; i < length; i++)
-{
-    if(array(i)>max)
-    {
-        max = array(i);
-        maxi = i;
-    }
-}
-static void RemoveAt(ref int[] array, int maxi)
-{
-    int[] newArray = new int[array.Length-1];
-    for (int i = 0; i<maxi; i++)
-        newArray[i] = array[i];
+// int[] array = { 1, 7, 9, 0 };
+// int length = array.Length;
+// int max = 0;
+// for (int i = 0; i < length; i++)
+// {
+//     if(array(i)>max)
+//     {
+//         max = array(i);
+//         maxi = i;
+//     }
+// }
+// static void RemoveAt(ref int[] array, int maxi)
+// {
+//     int[] newArray = new int[array.Length-1];
+//     for (int i = 0; i<maxi; i++)
+//         newArray[i] = array[i];
 
-    for(int i = maxi +1; i < array.Length; i++)
-        newArray[i-1] = array[i];
+//     for(int i = maxi +1; i < array.Length; i++)
+//         newArray[i-1] = array[i];
     
-    array = newArray;
-}
+//     array = newArray;
+// }
 
-for (int i = 0; i < length-1; i++)
+// for (int i = 0; i < length-1; i++)
+// {
+//     if(newArray[i]>max)
+//     {
+//         max = newArray[i];
+//     }
+// }
+// Console.WriteLine(max);
+
+// тупой способ решения
+
+// Console.Clear();
+// int x = 1;
+// int x1 = 7;
+// int x2 = 9;
+// int x3 = 0;
+// int max = x;
+// int max1 = 0;
+// if(x1>max)
+// {
+//     max = x1;
+// }
+
+// if (x2>max)
+// {
+//     max = x2;
+// }
+
+// if(x3>max)
+// {
+//     max = x3;
+// }
+
+// Console.WriteLine(max);
+
+// if(x > max1 && x != max)
+//     max1 = x;
+
+// if(x1 > max1 && x1 != max)
+//     max1 = x1;
+
+// if(x2 > max1 && x2 != max)
+//     max1 = x2;
+
+// if(x3 > max1 && x3 != max)
+//     max1 = x3;
+
+// Console.WriteLine(max1);
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int x = int.Parse(Console.ReadLine()!);
+int max = 0;
+int max1 = 0;
+while(x != 0)
 {
-    if(newArray(i)>max)
+    if(x > max)
     {
-        max = newArray(i);
+        max = x;
     }
-}
-Console.WriteLine(max);
 
+    if(x > max1 || x < max)
+    {
+        max1 = x;
+    }
+    Console.WriteLine("Введите число: ");
+    x = int.Parse(Console.ReadLine()!);
+}
+Console.WriteLine($"Второе по величине число {max1}");
